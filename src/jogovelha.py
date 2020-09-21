e817edcede2269d0c3fbe8703b553f459119ebbd
Enricky9 <enricky.nascimento@aluno.faculdadeimpacta.com.br>
2020-09-20 21:38:14 -0300

    Travis

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -13,26 +13,4 @@ def main( ):
 
 if name == "main":
     main()
-o conteudo do arquivo testes.py é 
-import jogovelha
-import sys
 
-erroInicializar = False
-jogo = jogovelha.inicializar()
-
-if len(jogo) != 3:
-    erroInicializar = True
-else:
-    for linha in jogo:
-        if len(linha) != 3:
-            erroInicializar = True
-        else:
-            for elemento in linha:
-                if elemento != '.':
-                    erroInicializar = True
-if erroInicializar:
-    sys.exit(1)
-else:
-    sys.exit(0)
-
-Conversar em #canal-de-estudo
